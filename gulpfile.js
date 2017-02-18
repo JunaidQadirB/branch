4ac9b544da1e611b0de91f9c9bdc24fea93beff2
Junaid Qadir Shekhanzai <shekhanzai.baloch@gmail.com>
2017-02-18 15:40:22 +0500

    QFE: Renamed sass to scss
    gulpfile.js: Code optimizitaions
    
    package.json: Added missing gulp extensions
    
    style.css: Added version

--- gulpfile.js
+++ gulpfile.js
@@ -2,79 +2,70 @@ var gulp = require('gulp');
 var notify = require('gulp-notify');
 var uglify = require('gulp-uglify');
 var cleanCss = require('gulp-clean-css');
-var sass = require('gulp-sass');
+var scss = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var rm = require('gulp-rm');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
-var livereload = require('gulp-livereload');
 var browserSync = require('browser-sync').create('Server');
-
-gulp.task('browser-sync', function () {
-    //watch files
-    var files = [
-        './style.css',
-        './*.php'
-    ];
-
-    //initialize browsersync
-    var url = "localhost/PostDemo/";
-    browserSync.init(files, {
-        //browsersync with a php server
-        proxy: url,
-        notify: false
-    });
-});
-
-/**
- * Compile SCSS files
- */
-
-gulp.task('sass', function () {
-    return gulp.src('assets/src/sass/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.init())
-        .pipe(cleanCss())
-        .pipe(sourcemaps.write())
-        .pipe(concat('all.min.css'))
-        .pipe(gulp.dest('assets/build/css/'))
-        .pipe(notify('SASS build complete!'))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('copyFonts', function () {
-    return gulp.src('node_modules/bootstrap-sass/assets/fonts/bootstrap/*')
-        .pipe(gulp.dest('assets/build/fonts/'))
-        .pipe(browserSync.stream());
-});
+var devUrl = "http://localhost/branch-test";
 
 gulp.task('clean:build', function () {
     return gulp.src('assets/build/**/*').pipe(rm());
 });
 
+gulp.task('scss', function () {
+    return gulp.src('assets/src/scss/*.scss')
+        .pipe(scss().on('error', scss.logError))
+        .pipe(sourcemaps.init())
+        .pipe(cleanCss())
+        .pipe(sourcemaps.write())
+        .pipe(concat('all.min.css'))
+        .pipe(gulp.dest('assets/build/css/'))
+        .pipe(notify('scss build complete!'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('fonts', function () {
+    return gulp.src('./node_modules/bootstrap-sass/assets/fonts/bootstrap/*')
+        .pipe(gulp.dest('assets/build/fonts/'))
+        .pipe(browserSync.stream());
+});
+
 gulp.task('js', function () {
     return gulp.src(
         [
-            'node_modules/jquery/dist/jquery.js',
-            'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
-            'assets/src/js/*.js'
+            './node_modules/jquery/dist/jquery.js',
+            './node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
+            './assets/src/js/*.js'
         ]
-    ).pipe(concat('all.min.js'))
-        .pipe(gulp.dest('assets/build/js'))
+    ).pipe(sourcemaps.init())
+        .pipe(concat('all.min.js'))
+        .pipe(sourcemaps.write())
+        .pipe(uglify())
+        .pipe(gulp.dest('./assets/build/js'))
         .pipe(notify('JS build complete!'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
     notify('Watch started!');
-    browserSync.init({
-        server: "/home/jeykeu/Code/wordpress/",
-
+    var files = [
+        './style.css',
+        './assets/src/scss/*.scss',
+        './assets/src/js/*.js',
+        './**/*.php',
+        './**/*.twig'
+    ];
+    browserSync.init(files, {
+        proxy: {
+            target: devUrl//Your wordpress URL
+        }
     });
-    gulp.watch('assets/src/sass/*.scss', ['sass']);
     gulp.watch('assets/src/js/*.js', ['js']);
-    gulp.watch(['copyFonts']);
-
+    gulp.watch('assets/src/scss/*.scss', ['scss']);
+    gulp.watch(['fonts']);
+    gulp.watch("*.twig").on('change', browserSync.reload);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['js', 'scss', 'fonts', 'watch']);
