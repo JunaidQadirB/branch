6c38c90b981dca45f04b98e9df3c1902332a2f35
Kaido <toomingas.k@gmail.com>
2018-05-18 19:12:14 +0300

    Update archive.php
    Fix archive templates pattern

--- archive.php
+++ archive.php
@@ -34,11 +34,11 @@ if (is_day()) {
             } else {
                 if (is_category()) {
                     $data['title'] = single_cat_title('', false);
-                    array_unshift($templates, 'archive-' . get_query_var('cat') . '.twig');
+                    array_unshift($templates, 'pages/archive-' . get_query_var('cat') . '.twig');
                 } else {
                     if (is_post_type_archive()) {
                         $data['title'] = post_type_archive_title('', false);
-                        array_unshift($templates, 'archive-' . get_post_type() . '.twig');
+                        array_unshift($templates, 'pages/archive-' . get_post_type() . '.twig');
                     }
                 }
             }
